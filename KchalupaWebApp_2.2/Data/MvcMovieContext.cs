4adea2a9738b4c460cb0b59bc73f182fe805d977
Kenny Chalupa <kechalup@microsoft.com>
2019-05-17 15:48:37 -0500

    Add project files.

---
+++ KchalupaWebApp_2.2/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KchalupaWebApp_2._2.Controllers;
+
+namespace KchalupaWebApp_2._2.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<KchalupaWebApp_2._2.Controllers.Movie> Movie { get; set; }
+    }
+}
